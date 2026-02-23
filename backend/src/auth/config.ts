7e5b6402f4e7dcf5d939f994a6024c375643d471
Tomasz Turek <turek9933@gmail.com>
2026-02-23 10:30:47 +0100

    Parametrization and schema interpretation fix

--- backend/src/auth/config.ts
+++ backend/src/auth/config.ts
@@ -7,10 +7,10 @@ export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "pg",
         schema: {
-            user: users,
-            session: sessions,
-            account: accounts,
-            verification: verifications,
+            users: users,
+            sessions: sessions,
+            accounts: accounts,
+            verifications: verifications,
         },
     }),
     user: {
@@ -21,8 +21,8 @@ export const auth = betterAuth({
     },
     session: {
         modelName: "sessions",
-        expiresIn: 60 * 60 * 24 * 14,// Session expires in 14 days
-        updateAge: 60 * 60 * 24 * 2,// Update session age every 2 days
+        expiresIn: (Number(process.env.SESSION_EXPIRATION_DAYS) ?? 14 ) * 60 * 60 * 24,// Session expires in (SESSION_EXPIRATION_DAYS or 14) days
+        updateAge: (Number(process.env.SESSION_UPDATE_DAYS) ?? 2 ) * 60 * 24,// Update session age every (SESSION_UPDATE_DAYS or 2) days
     },
     account: { modelName: "accounts" },
     verification: { modelName: "verifications" },
@@ -35,4 +35,7 @@ export const auth = betterAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         },
     },
+    trustedOrigins: [
+        process.env.CORS_ORIGIN!
+    ],
 });
